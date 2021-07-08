80388313d8c0fc6977919391213d836267698efd
Daniel Tavares <dantavares@gmail.com>
2021-07-08 18:28:32 -0300

    Update radio.php

--- radio.php
+++ radio.php
@@ -1,8 +1,8 @@
 <?php
     $db_host = 'localhost';
     $db_name = 'radio';
-    $db_user = 'radio';
-    $db_pass = 'issdnata';
+    $db_user = 'db_user';
+    $db_pass = 'db_passwd';
     
     $db = mysqli_connect($db_host,$db_user,$db_pass,$db_name);
     if(!$db) { die("Erro ao conectar no banco de dados: " . mysqli_connect_error()); }
