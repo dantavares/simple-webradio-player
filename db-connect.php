062cf992310c8177dfe6f7581927609d9c1dd4c7
Daniel Tavares <dantavares@gmail.com>
2023-08-10 14:10:22 -0300

    Update db-connect.php

--- db-connect.php
+++ db-connect.php
@@ -1,8 +1,8 @@
 <?php
-    $db_host = 'localhost';
-    $db_name = 'yourdbname';
-    $db_user = 'yourdbusername';
-    $db_pass = 'yourdbpassword';
+    $db_host = 'your db host';
+    $db_name = 'your db name';
+    $db_user = 'your db username';
+    $db_pass = 'your db password';
     
     $db = mysqli_connect($db_host,$db_user,$db_pass,$db_name);
     if(!$db) { die("Erro ao conectar no banco de dados: " . mysqli_connect_error()); }
